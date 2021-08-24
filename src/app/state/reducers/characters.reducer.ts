e61c05d7b0be5a75358018f670ffe27309d1327f
klodian <dev.klodian@gmail.com>
2021-08-25 01:57:12 +0200

    handle delete edit and load function on reducer

--- src/app/state/reducers/characters.reducer.ts
+++ src/app/state/reducers/characters.reducer.ts
@@ -1,6 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import {CharacterModel} from "../../shared/models/character.model";
-import {loadCharacters} from "../actions/characters.actions";
+import {deleteCharacter, editCharacter, loadCharacters} from "../actions/characters.actions";
 
 export const charactersFeatureKey = 'characters';
 
@@ -14,6 +14,11 @@ export const initialState: CharactersState = {
 
 export const charactersReducer = createReducer(
   initialState,
-  on(loadCharacters, (state, {characters}) => ( {characters: characters}))
+  on(loadCharacters, (state, {characters}) => ( {characters: characters})),
+  on(deleteCharacter, (state, {id} ) => ({characters: state.characters.filter(character => character.id != id)})),
+  on(editCharacter, (state, {character} ) => ({characters: state.characters.map( item => {
+    if(item.id == character.id){ return {...item, ...character}}
+    return item;
+  })})),
 );
 
